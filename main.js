847d4aa908a7764bffb4676d21480ab495901420
unknown <syyadkhan@yahoo.com>
2018-09-29 09:43:11 -0400

    Finished Project

--- main.js
+++ main.js
@@ -1,42 +1,83 @@
-const TypeWriter = function(txtElement, words, wait = 3000) {
-  this.txtElement = txtElement;
-  this.words = words;
-  this.txt = "";
-  this.wordIndex = 0;
-  this.wait = parseInt(wait, 10);
-  this.type();
-  this.isDeleting = false;
-};
+// const TypeWriter = function(txtElement, words, wait = 3000) {
+//   this.txtElement = txtElement;
+//   this.words = words;
+//   this.txt = "";
+//   this.wordIndex = 0;
+//   this.wait = parseInt(wait, 10);
+//   this.type();
+//   this.isDeleting = false;
+// };
 
-TypeWriter.prototype.type = function() {
-  const current = this.wordIndex % this.words.length;
-  const fullTxt = this.words[current];
+// TypeWriter.prototype.type = function() {
+//   const current = this.wordIndex % this.words.length;
+//   const fullTxt = this.words[current];
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+//   if (this.isDeleting) {
+//     this.txt = fullTxt.substring(0, this.txt.length - 1);
+//   } else {
+//     this.txt = fullTxt.substring(0, this.txt.length + 1);
+//   }
 
-  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+//   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-  let typeSpeed = 300;
+//   let typeSpeed = 300;
 
-  if (this.isDeleting) {
-    typeSpeed /= 2;
-  }
+//   if (this.isDeleting) {
+//     typeSpeed /= 2;
+//   }
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    typeSpeed = this.wait;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
+//   if (!this.isDeleting && this.txt === fullTxt) {
+//     typeSpeed = this.wait;
+//     this.isDeleting = true;
+//   } else if (this.isDeleting && this.txt === "") {
+//     this.isDeleting = false;
+//     this.wordIndex++;
+//     typeSpeed = 500;
+//   }
+
+//   setTimeout(() => this.type(), typeSpeed);
+// };
+
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
     this.isDeleting = false;
-    this.wordIndex++;
-    typeSpeed = 500;
   }
+  type() {
+    const current = this.wordIndex % this.words.length;
+    const fullTxt = this.words[current];
 
-  setTimeout(() => this.type(), typeSpeed);
-};
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    let typeSpeed = 300;
+
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      typeSpeed = this.wait;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.wordIndex++;
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
 
 document.addEventListener("DOMContentLoaded", init);
 
